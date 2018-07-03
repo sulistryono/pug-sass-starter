5f586dabe86d30b777ce5995d3c20935637b5050
Sulis Triyono <sulis.tryono@gmail.com>
2018-07-03 09:57:55 +0700

    Update gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -14,44 +14,44 @@ gulp.task('connect', function() {
 });
 
 gulp.task('styles', function() {
-  gulp.src('./src/sass/styles.scss')
+  gulp.src('src/sass/styles.scss')
   .pipe(plumber())
   .pipe(sass({outputStyle: 'compressed'}))
   .pipe(rename('styles.min.css'))
-  .pipe(gulp.dest('./assets/css'))
+  .pipe(gulp.dest('assets/css'))
   .pipe(connect.reload());
 });
 
 gulp.task('scripts', function() {
-  gulp.src('./src/js/scripts.js')
+  gulp.src('src/js/scripts.js')
   .pipe(plumber())
   .pipe(uglify())
   .pipe(rename('scripts.min.js'))
-  .pipe(gulp.dest('./assets/js'))
+  .pipe(gulp.dest('assets/js'))
   .pipe(connect.reload());
 });
 
 gulp.task('html', function() {
-  gulp.src('./*.html')
+  gulp.src('*.html')
   .pipe(plumber())
   .pipe(connect.reload());
 });
 
 gulp.task('views', function buildHTML() {
-  return gulp.src('./src/pug/pages/*.pug')
+  return gulp.src('src/pug/pages/*.pug')
   .pipe(plumber())
   .pipe(pug({
       pretty: true
   }))
-  .pipe(gulp.dest('./'))
+  .pipe(gulp.dest(''))
   .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['./*.html'], ['html']);
-  gulp.watch(['./src/sass/**/*.scss'], ['styles']);
-  gulp.watch(['./src/js/scripts.js'], ['scripts']);
-  gulp.watch(['./src/pug/**/*.pug'], ['views']);
+  gulp.watch(['*.html'], ['html']);
+  gulp.watch(['src/sass/**/*.scss'], ['styles']);
+  gulp.watch(['src/js/scripts.js'], ['scripts']);
+  gulp.watch(['src/pug/**/*.pug'], ['views']);
 });
 
 gulp.task('default', ['connect', 'styles', 'scripts', 'html', 'views', 'watch']);
